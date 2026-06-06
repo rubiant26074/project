8451b0e4f212e7eae04e3d124b8ed55a4ce683b1
rubiant26074 <haep0506@gmail.com>
2026-06-06 08:57:18 +0700

    Fix user mass assignment on hosting

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,20 +4,31 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Models\ProjectProcess;
 
-#[Fillable(['name', 'email', 'role', 'password'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'role',
+        'password',
+        'is_active',
+        'approved_at',
+        'approved_by',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
     /**
      * Get the attributes that should be cast.
